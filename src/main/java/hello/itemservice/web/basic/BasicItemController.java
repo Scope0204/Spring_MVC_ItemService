3461000ad8f343afdc62c36a6efaef435a8ba54c
Scope0204 <scope0204@gmail.com>
2022-04-01 13:09:51 +0900

    상품 상세

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -26,12 +27,19 @@ public class BasicItemController {
         return "basic/items";
     }
 
+    @GetMapping({"/{itemId}"})
+    public String item(@PathVariable long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
     @PostConstruct
     public void init(){
         itemRepository.save(new Item("itemA", 10000,10));
-        itemRepository.save(new Item("itemB", 20000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
     }
 }
