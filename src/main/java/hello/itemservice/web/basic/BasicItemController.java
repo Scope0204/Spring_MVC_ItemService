f6c533e5f56ca3c6909c1abfdcee7cb7cb101220
Scope0204 <scope0204@gmail.com>
2022-04-04 20:24:25 +0900

    RedirectAttributes + thymeleaf 쿼리파라미터 조회

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -82,10 +83,22 @@ public class BasicItemController {
     /**
      * 상품 등록 후 뷰 템플릿이 아니라, 상품 상세목록으로 리다이렉트(PRG)
      */
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV5(Item item){
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
+    }
+
+    /**
+     * v5 + 상품 저장시 뷰 템플릿에 "저장완료" 추가
+     */
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        // url에 없는 경우, 쿼리파라미터 형식으로 들어가게 됨 => ?status=true
+        return "redirect:/basic/items/{itemId}";
     }
 
 
